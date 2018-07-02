4d6f5a6cdf82defa80a000508c750a2ed5202a4a
mattrudin <matthias.rudin@live.com>
2018-07-02 04:59:45 +0200

    Added plan of action

--- main.js
+++ main.js
@@ -4,5 +4,9 @@ preserving the order of the other elements.
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0] */
 
 var moveZeros = function (arr) {
-  // TODO: Program me
+  // 1. get the length of array
+  // 2. .filter out all zeros
+  // 3. get the length of the array without zeros
+  // 4. calculate the difference between the original array and the new array
+  // 5. push zeros into the new array: qty according step 4
 }
